ebdd306aaa20b00d47f48344b27cc870dc0c2536
subashnandigam <subashnandigam7@gmail.com>
2023-08-15 15:46:28 -0400

    adde the files

---
+++ quizzapp/src/test/java/com/subhash/quizzapp/QuizzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhash.quizzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
